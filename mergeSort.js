559979e668426998b36152d380cad4bf963a6204
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-07-13 17:19:16 -0300

    finish mergeSort function, use random numbers to order
    The function was done wihtout even noticing while making the
    order function. Random numbers are put on the list before
    starting the test.

--- mergeSort.js
+++ mergeSort.js
@@ -1,37 +1,35 @@
-const array = [7, 5, 9, 2, 6, 1, 3, 8, 0];
+const array = [];
 
-// function mergeSort (arr) {
-//   const left = arr.slice(0, arr.length / 2);
-//   const right = arr.slice((arr.length / 2));
+for (let i = 0; i < 10; i++) {
+  array.push(Math.floor(Math.random() * 100));
+}
 
-//   if (arr.length > 2) {
-//     console.log(arr.join(' - '));
-//     console.log(left, right);
-//     mergeSort(left);
-//     mergeSort(right);
-//   }
+function mergeSort (arr) {
+  const left = arr.slice(0, arr.length / 2);
+  const right = arr.slice((arr.length / 2));
 
-//   return sort(left, right);
-// }
+  if (arr.length > 2) {
+    mergeSort(left);
+    mergeSort(right);
+  }
+
+  return sort(left, right);
+}
 
 function sort (left, right) {
-  console.log("lists: ", left, right);
   const ordered = [];
   
   while (left.length > 0 || right.length > 0) {
     const lowerLeftIdx = left.indexOf(Math.min(...left));
     const lowerRightIdx = right.indexOf(Math.min(...right));
-    console.log("L: ", left[lowerLeftIdx], " | R: ", right[lowerRightIdx]);
     let lower = left[lowerLeftIdx] < right[lowerRightIdx] || right.length === 0
       ? left.splice(lowerLeftIdx, 1)[0]
       : right.splice(lowerRightIdx, 1)[0];
-    console.log("lower: ", lower);
     ordered.push(lower);
-    console.log("ordered: ", JSON.stringify(ordered));
-  } ;
+  };
 
   return ordered;
 }
 
-// console.log(mergeSort(array));
-sort([7, 5, 9, 2],[6, 1, 3, 8, 0]);
+console.log('unordered list:', ...array);
+console.log('Ordered list: ', ...mergeSort(array));
